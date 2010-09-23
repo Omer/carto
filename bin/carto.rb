215013d3074dad51ecdfb42638d55079157a9e92
shearn89 <alexander.shearn@gmail.com>
2010-09-23 13:42:11 +0100

    Updating - parse the xml file -> yaml.
    Updating to add in functionality to parse the xml file into a more
    ruby-friendly yaml file. May not be needed, waiting to see xoebus'
    version...

--- bin/carto.rb
+++ bin/carto.rb
@@ -9,6 +9,7 @@ APP_ROOT = File.join(File.dirname(Pathname.new(__FILE__).realpath),'/..')
 LIB_ROOT = APP_ROOT + '/lib'
 
 PROFILENAME = APP_ROOT + '/hosts/inventory.xml'
+YAMLNAME = APP_ROOT + '/hosts/inventory.yaml'
 
 puts ">>> Carto library loaded. Getting information..."
 def self.get_inventory
@@ -21,22 +22,36 @@ def self.get_inventory
 			}
 		}
 		puts ">>> Reacquisition complete."
+		return parse_me( Document.new(File.new(PROFILENAME)) )
 	end
 	puts ">>> Inventory loaded. <<<"
-	return Document.new(File.new(PROFILENAME))
+	return File.new(YAMLNAME)
+end
+
+def self.parse_me(invo)
+	output = "---\n"
+	invo.elements.each('inventory/node') { |element| 
+		machine_loc = element.elements["location"].text
+		unless machine_loc.nil?
+			if machine_loc[0..1] == "AT"
+				machine_floor = machine_loc[3,1]
+				machine_room = machine_loc[5..-1]
+				machine_name = element.attributes["name"]
+				output << "name:\t#{machine_name}\n\tfloor:\t#{machine_floor}\n\troom:\t#{machine_room}\n"
+			end
+		end
+	}
+	open(YAMLNAME, 'w') {|file|
+		file.write(output)
+	}
+	return File.new(YAMLNAME)
 end
 
 def self.inventory
 	@inventory ||= get_inventory
 end
 
-inventory()
-@inventory.elements.each('inventory/node') { |element| 
-	machine_loc = element.elements["location"].text
-	unless machine_loc.nil?
-		if machine_loc[0..1] == "AT"
-			puts "#{element.attributes["name"]}:#{machine_loc}"
-		end
-	end
-	}
+inventory
+puts ">>> Loading complete. Carto initialized and ready..."
 
+puts @inventory
